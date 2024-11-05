b9a6252e372589944f836c22a5dda083f1784df6
elliotA1ders0n <130219411+elliotA1ders0n@users.noreply.github.com>
2024-11-05 13:07:36 +0100

    WEB file-ok frissítése

---
+++ WEB/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Főoldal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<?php require "navbar.html";?>
+
+  <?php require "bookList.html";?>
+
+  <?php require "footer.html";?>
+
+<script src="main.js"></script>
+
+
+
+
+
+
+</body>
+</html>
---
+++ WEB/main.js
@@ -0,0 +1,83 @@
+/* navbar script */
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.add("show-menu");
+});
+
+navClose.addEventListener("click", () => {
+  navMenu.classList.remove("show-menu");
+});
+
+const search = document.getElementById("search"),
+  searchBtn = document.getElementById("search-btn"),
+  searchClose = document.getElementById("search-close");
+
+searchBtn.addEventListener("click", () => {
+  search.classList.add("show-search");
+});
+
+searchClose.addEventListener("click", () => {
+  search.classList.remove("show-search");
+});
+
+/* window.onclick = function(event) {
+    if (event.target == search) {
+        search.classList.remove("show-search");
+        document.body.style.overflow = "auto";
+    }
+} */
+
+const login = document.getElementById("login"),
+  loginBtn = document.getElementById("login-btn"),
+  loginClose = document.getElementById("login-close");
+
+loginBtn.addEventListener("click", () => {
+  login.classList.add("show-login");
+});
+
+loginClose.addEventListener("click", () => {
+  login.classList.remove("show-login");
+});
+
+/* window.onclick = function(event) {
+    if (event.target == login) {
+        login.classList.remove("show-login");
+        document.body.style.overflow = "auto";
+    }
+} */
+
+
+/* LOGIN SCRIPT */
+
+const passwordField = document.getElementById("password");
+const togglePassword = document.querySelector(".password-toggle-icon i");
+
+togglePassword.addEventListener("click", function () {
+    if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+    } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+    }
+});
+
+
+
+
+/* function showGenres() {
+	var x = document.getElementById("choose_genres");
+	if (x.style.display === "none"  || x.style.display === "") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+} */
+
+
