40ae2e2750fc1cc15723b4632dc9c0cd0d624db0
BaBence04 <160581232+BaBence04@users.noreply.github.com>
2025-01-10 13:26:31 +0100

    ApiComm class added

---
+++ DESKTOP/Desktop/ApiComm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace Desktop
+{
+    internal class ApiComm
+    {
+        public static async Task<object> SendPost()
+        {
+            var client = new HttpClient();
+            var values = new Dictionary<string, string> { { "test", "ping" } };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("http://localhost:84/Konyvtarazo-rendszer-projekt-main/BACKEND/api.php", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+            client.Dispose();
+        }
+
+    }
+}
