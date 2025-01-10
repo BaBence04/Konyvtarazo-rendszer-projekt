7ff6eb7a33dc4ff29ac8f8e61d14fd0ef1fae253
hnblnt <hortibalint@gmail.com>
2025-01-10 13:21:15 +0100

    Update api.php

--- BACKEND/api.php
+++ BACKEND/api.php
@@ -1,6 +1,8 @@
 <?php
     require_once "databaseFunctions.php";
     require "elementCreators.php";
+    
+    session_start();
     //listBooksFiltered
 
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
@@ -49,10 +51,19 @@
 
             echo json_encode($result);
             
+        }else if(isset($_POST["action"]) && count($_POST) == 1 && $_POST["action"] == "isLoggedIn"){
+            echo isset($_SESSION['user_id']) ? $_SESSION["user_id"] : "";
+
+        }else if(isset($_SESSION["user_id"], $_POST["action"]) && $_POST["action"] == "logout"){
+            unset($_SESSION["user_id"]);
+
+            
         }else{
             throw new Exception("Nincs ilyen");
         }
     }
+
+    session_commit()
 
 
 
