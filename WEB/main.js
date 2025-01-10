1167395d840a86da82562810586c7b0fd3457b81
hnblnt <hortibalint@gmail.com>
2025-01-10 13:21:40 +0100

    Update main.js

--- WEB/main.js
+++ WEB/main.js
@@ -1,6 +1,6 @@
 /* navbar script */
 
-const navMenu = document.getElementById("nav-menu");
+// var navMenu = document.getElementById("nav-menu");
 var navToggle = document.getElementById("nav-toggle");
 var navClose = document.getElementById("nav-close");
 
@@ -35,13 +35,7 @@ const login = document.getElementById("login"),
   loginBtn = document.getElementById("login-btn"),
   loginClose = document.getElementById("login-close");
 
-loginBtn.addEventListener("click", () => {
-  login.classList.add("show-login");
-});
 
-loginClose.addEventListener("click", () => {
-  login.classList.remove("show-login");
-});
 
 /* window.onclick = function(event) {
     if (event.target == login) {
@@ -49,17 +43,55 @@ loginClose.addEventListener("click", () => {
         document.body.style.overflow = "auto";
     }
 } */
-
+let waitingforServer = false;
 
 /* LOGIN SCRIPT */
+loginBtn.addEventListener("click", () => {
+  if(waitingforServer){
+    return;
+  }
+
+  waitingforServer = true;
+
+  $.ajax({
+    url: "../BACKEND/api.php",
+    type: "post", //send it through get method
+    data: { 
+        action: "isLoggedIn"
+    },
+    success: function(response)  {
+      console.log(response)
+      if(response == ""){
+        login.classList.add("show-login");
+        waitingforServer = false;
+
+      }else{
+        window.open("./?page=userDetailed", "_self");
+        // const currentUrl = new URL("./");
+        // currentUrl.searchParams.set("page", "userDetailed");
+        // // console.log(currentUrl)
+        // window.history.pushState({}, '', currentUrl);
+        // location.reload();
+      }
+    }
+    });
+
+
+
+});
+
+loginClose.addEventListener("click", () => {
+  login.classList.remove("show-login");
+});
+
 
 const passwordField = document.getElementById("password");
-const togglePassword = document.getElementById("#eyeicon");
+const togglePassword = document.getElementById("eyeicon");
 
 togglePassword.addEventListener("click", function () {
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        togglePassword.src = "imgs/eye-line.png";
+        togglePassword.src = "./imgs/eye-line.png";
     } else {
         passwordField.type = "password";
         togglePassword.src = "imgs/eye-off-line.png";
@@ -70,11 +102,49 @@ togglePassword.addEventListener("click", function () {
 function HideAndShowFilters(){
   const additionalFilters = document.getElementById("additional-filters");
 
-  if (additionalFilters.style.display === "none" || additionalFilters.style.display === "") {
-    additionalFilters.style.display = "grid";
+  jQuery.easing.def = "easeInCubic";
+  let duration = 400;
+  if (additionalFilters.getAttribute("data-open") == "0") {
+    // additionalFilters.style.display = "grid";
+    $(additionalFilters).slideDown(duration, "swing");
+    additionalFilters.setAttribute("data-open", "1");
   } else {
-    additionalFilters.style.display = "none";
+    // additionalFilters.style.display = "none";
+    $(additionalFilters).slideUp(duration, "swing");
+    additionalFilters.setAttribute("data-open", "0");
+  }
 }
+
+function CheckLogin(){
+  console.log(document.getElementById("username").value, document.getElementById("password").value )
+  $.ajax({
+    url: "../BACKEND/api.php",
+    type: "POST", //send it through get method
+    data: { 
+        uname: document.getElementById("username").value, 
+        pw: document.getElementById("password").value 
+    },
+    success: function(response)  {
+        //ALERTEKET VALAMI MÁS LESZ MAJD EZ CSAK ÁTMENETI
+        if(response == "not found"){
+            alert("helytelen felhasználónév")
+        }else if(response == "inactive user"){
+            alert("ez a felhasználó inaktív")
+        }else if(response == "incorrect"){
+            alert("helytelen Jelszó")
+        }else{
+            //HA LESZ FŐOLDAL AKKOR ODA IRÁNYÍTSON
+            // location.replace("bookList.php")
+            // alert("sikeres bejelentkezés")
+            login.classList.remove("show-login");
+        }
+    
+    }}); 
 }
+
+// window.addEventListener('popstate', function (event) {
+//   // Code to refresh or reload content when the back button is clicked
+//   location.reload();
+// });
 
 
