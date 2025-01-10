29e9ed1bc58efb78c03920b47c339b559d13d2be
hnblnt <hortibalint@gmail.com>
2025-01-10 13:21:34 +0100

    Create bookListFilters.php

---
+++ WEB/bookListFilters.php
@@ -0,0 +1,41 @@
+<div id="filters">
+<div id="baseFilters">
+
+    <div class="title-filter">
+        <label for="title">Cím</label>
+        <input type="text" id="title" value="" name="title" placeholder="pl.:  Állatfarm">
+    </div>
+    
+    <button id="toggleFilters" type="button" onclick="HideAndShowFilters();">További szűrők</button>
+</div>
+
+<div id="additional-filters" data-open="0" style="display: none;">
+    <?=CreateGenreFilter()?>
+    
+    <div class="author-filter">
+        <label for="author">Szerző</label>
+        <input type="text" id="author" name="author" placeholder="pl.:  George Orwell">
+    </div>
+    
+    <div class="releaseDate-filter">
+        <label for="date">Megjelenési dátum</label>
+        <input type="date" id="date" name="date">
+    </div>
+    
+    <?=CreateLanguageFilter()?>
+    
+    <div class="ISBN-filter">
+        <label for="ISBN">ISBN-kód</label>
+        <input type="text" id="ISBN" name="ISBN">
+    </div>
+    
+    
+</div>
+
+
+
+
+<input id="searchButton" type="button" onclick="applyFilters()" value="Keresés">
+
+
+</div>
