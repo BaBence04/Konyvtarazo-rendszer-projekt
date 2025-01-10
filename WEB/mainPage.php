10000d511c020c6327d08725d6e2f86286727c4a
hnblnt <hortibalint@gmail.com>
2025-01-10 13:21:44 +0100

    Update mainPage.php

--- WEB/mainPage.php
+++ WEB/mainPage.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Könytvtár</title>
-</head>
+
 <body>
 
     <section class="hero main_section">
@@ -58,4 +51,3 @@
 
 
 </body>
-</html>
