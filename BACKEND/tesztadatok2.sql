01063782784d6966fd80546d110b86492463413d
hnblnt <hortibalint@gmail.com>
2025-01-16 19:00:27 +0100

    Update tesztadatok2.sql

--- BACKEND/tesztadatok2.sql
+++ BACKEND/tesztadatok2.sql
@@ -54,21 +54,23 @@ INSERT INTO book (ISBN, title, publisher_id, release_date, lang_id, description,
 ('9789999999999', 'Book Fourteen', 3, '2020-12-10', 2, 'A chilling horror story with supernatural elements.', "https://unsplash.it/300/450"),
 ('9781010101010', 'Book Fifteen', 1, '2022-07-14', 5, 'A biography of a renowned scientist.', "https://unsplash.it/300/450"),
 
-('9781234567897', 'Első könyv', 1, '2020-01-15', 1, 'Egy lebilincselő regény.', "https://unsplash.it/300/450"),
-('9789876543210', 'Második könyv', 2, '2019-07-23', 2, 'Egy gondolatébresztő tudományos könyv.', "https://unsplash.it/300/450"),
-('9785556667778', 'Harmadik könyv', 3, '2021-03-05', 3, 'Egy epikus sci-fi történet.', "https://unsplash.it/300/450"),
-('9784445556667', 'Negyedik könyv', 4, '2022-10-12', 4, 'Egy lenyűgöző fantasy kaland.', "https://unsplash.it/300/450"),
-('9783334445556', 'Ötödik könyv', 5, '2018-05-19', 5, 'Egy izgalmas krimi regény.', "https://unsplash.it/300/450"),
-('9781111111111', 'Hatodik könyv', 2, '2023-01-20', 1, 'Egy romantikus történet kontinenseken át.', "https://unsplash.it/300/450"),
-('9782222222222', 'Hetedik könyv', 4, '2021-09-10', 4, 'Egy horror történet, ami ébren tart.', "https://unsplash.it/300/450"),
-('9783333333333', 'Nyolcadik könyv', 1, '2020-06-15', 2, 'Egy híres történelmi alak életrajza.', "https://unsplash.it/300/450"),
-('9784444444444', 'Kilencedik könyv', 3, '2019-11-22', 3, 'Egy sci-fi remekmű a mesterséges intelligenciáról.', "https://unsplash.it/300/450"),
-('9785555555555', 'Tizedik könyv', 5, '2022-04-05', 5, 'Egy kalandos fantasy egy varázslatos világban.', "https://unsplash.it/300/450");
+('9781234567887', 'Első könyv', 1, '2020-01-15', 1, 'Egy lebilincselő regény.', "https://unsplash.it/300/450"),
+('9789876541210', 'Második könyv', 2, '2019-07-23', 2, 'Egy gondolatébresztő tudományos könyv.', "https://unsplash.it/300/450"),
+('9785556667770', 'Harmadik könyv', 3, '2021-03-05', 3, 'Egy epikus sci-fi történet.', "https://unsplash.it/300/450"),
+('2784445556667', 'Negyedik könyv', 4, '2022-10-12', 4, 'Egy lenyűgöző fantasy kaland.', "https://unsplash.it/300/450"),
+('9783334445552', 'Ötödik könyv', 5, '2018-05-19', 5, 'Egy izgalmas krimi regény.', "https://unsplash.it/300/450"),
+('9781111131111', 'Hatodik könyv', 2, '2023-01-20', 1, 'Egy romantikus történet kontinenseken át.', "https://unsplash.it/300/450"),
+('9782222122222', 'Hetedik könyv', 4, '2021-09-10', 4, 'Egy horror történet, ami ébren tart.', "https://unsplash.it/300/450"),
+('9783343333333', 'Nyolcadik könyv', 1, '2020-06-15', 2, 'Egy híres történelmi alak életrajza.', "https://unsplash.it/300/450"),
+('9784444344444', 'Kilencedik könyv', 3, '2019-11-22', 3, 'Egy sci-fi remekmű a mesterséges intelligenciáról.', "https://unsplash.it/300/450"),
+('9785552555555', 'Tizedik könyv', 5, '2022-04-05', 5, 'Egy kalandos fantasy egy varázslatos világban.', "https://unsplash.it/300/450");
 
 
 -- Adatok beszúrása a books_genres_conn táblába
 INSERT INTO books_genres_conn (ISBN_id, genre_id) VALUES
 (1, 1), -- Regény
+(1, 2),
+(1, 3),
 (2, 2), -- Tudományos
 (3, 3), -- Sci-fi
 (4, 4), -- Fantasy
@@ -77,7 +79,22 @@ INSERT INTO books_genres_conn (ISBN_id, genre_id) VALUES
 (7, 7), -- Horror
 (8, 8), -- Életrajz
 (9, 3), -- Sci-fi
-(10, 4); -- Fantasy
+(10, 4),
+(11, 2),
+(12, 2),
+(13, 2),
+(14, 2),
+(15, 2),
+(16, 2),
+(17, 2),
+(18, 2),
+(19, 2),
+(20, 2),
+(21, 2),
+(22, 2),
+(23, 2),
+(24, 2),
+(25, 2);
 
 -- Adatok beszúrása a books_authors_conn táblába
 INSERT INTO books_authors_conn (ISBN_id, author_id) VALUES
@@ -90,7 +107,26 @@ INSERT INTO books_authors_conn (ISBN_id, author_id) VALUES
 (7, 4), -- Stephen King
 (8, 6), -- Jane Austen
 (9, 5), -- Isaac Asimov
-(10, 2); -- George R.R. Martin
+(10, 2),
+(10, 3),
+(10, 4),
+(10, 5),
+(10, 6),
+(11, 2),
+(12, 2),
+(13, 2),
+(14, 2),
+(15, 2),
+(16, 2),
+(17, 2),
+(18, 2),
+(19, 2),
+(20, 2),
+(21, 2),
+(22, 2),
+(23, 2),
+(24, 2),
+(25, 2); -- George R.R. Martin
 
 
 INSERT INTO system_settings (membership_fee, borrowing_time, lengthening_time, reservation_time, max_reservations, max_lengthenings, latency_fee) VALUES
@@ -99,32 +135,44 @@ INSERT INTO system_settings (membership_fee, borrowing_time, lengthening_time, r
 
 INSERT INTO users (surname, first_name, username, birth_date, membership_end_date, email, phone_number, password, birth_place, address, mother_maiden_name, active) VALUES
 ('Kovács', 'János', '7584937648', '1985-03-15', '2025-03-15', 'janos.kovacs@example.com', '06123456789', 'password', 'Budapest', '123 Main St', 'Nagy Erzsébet', TRUE),
+('K.', 'Anna', '0123456789', '1999-10-12', '2025-10-15', 'k.anna@gmail.com', '06302166102', 'password', 'Vác', '123 Váci St', 'Kipuf Ogó', TRUE),
 ('Szabó', 'Anna', '6453957465', '1990-10-22', '2025-10-22', 'anna.szabo@example.com', '06789123456', 'password', 'Debrecen', '456 High St', 'Kiss Júlia', TRUE);
 
-INSERT INTO inventory (ISBN_id, available) VALUES
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(3, TRUE),
-(1, TRUE),
-(1, TRUE),
-(6, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(4, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(1, TRUE),
-(5, TRUE),
-(1, TRUE),
-(7, TRUE),
+INSERT INTO inventory (ISBN_id, book_id,available) VALUES
+(1,1,TRUE),(2,2,TRUE),(3,3,TRUE),(4,4,TRUE),(5,5,TRUE),(6,6,TRUE),(7,7,TRUE),(8,8,TRUE),(9,9,TRUE),(10,10,TRUE),(11,11,TRUE),(12,12,TRUE),(13,13,TRUE),(14,14,TRUE),(15,15,TRUE),(16,16,TRUE),(17,17,TRUE),(18,18,TRUE),(19,19,TRUE),(20,20,TRUE),(21,21,TRUE),(22,22,TRUE),(23,23,TRUE),(24,24,TRUE),(25,25,TRUE);
 
-(2, TRUE),
-(1,TRUE),(2,TRUE),(3,TRUE),(4,TRUE),(5,TRUE),(6,TRUE),(7,TRUE),(8,TRUE),(9,TRUE),(10,TRUE),(11,TRUE),(12,TRUE),(13,TRUE),(14,TRUE),(15,TRUE),(16,TRUE),(17,TRUE),(18,TRUE),(19,TRUE),(20,TRUE),(21,TRUE),(22,TRUE),(23,TRUE),(24,TRUE),(25,TRUE)
-;
+INSERT INTO employees (empl_name, empl_uname, password) VALUES
+('Tóth Péter', 'AABBC', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19'),
+('Nagy László', 'BBCCD', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19');
+
+INSERT INTO reservation (ISBN_id, user_id) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(1, 3),
+(2, 2),
+(3, 2);
+
+INSERT INTO booking (book_id, user_id, end_date) VALUES
+(1, 2, '2025-02-01'),
+(2, 2, '2025-02-01'),
+(3, 3, '2025-02-01'),
+(4, 3, '2025-02-01'),
+(5, 3, '2025-02-01'),
+(6, 1, '2025-02-15');
+
+INSERT INTO borrowings (book_id, user_id, empl_e, start_date, end_date, returned) VALUES
+(1, 1, 1, '2024-01-01', '2024-01-16', 1),
+(1, 1, 1, '2024-02-01', '2024-02-16', 1),
+
+(3, 1, 1, '2024-02-03', '2024-02-18', 0),
+(4, 1, 1, '2024-10-01', '2024-10-16', 0),
+(5, 1, 1, '2024-01-01', '2024-01-16', 0),
+(6, 1, 1, '2024-01-01', '2024-01-16', 0),
+(7, 1, 1, '2024-02-01', '2024-02-16', 0),
+(8, 1, 1, '2024-01-01', '2024-01-16', 0),
+(9, 1, 1, '2024-01-01', '2024-01-16', 0),
+(10, 1, 1, '2024-03-01', '2024-03-16', 1),
+(11, 1, 1, '2024-01-01', '2024-01-16', 1),
+
+(2, 2, 2, '2024-02-01', '2024-02-16', 1);
