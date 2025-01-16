5d518560061111611e660d44b59762197bb4310e
hnblnt <hortibalint@gmail.com>
2025-01-16 19:00:24 +0100

    Delete filterBooks.php

--- BACKEND/filterBooks.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require "elementCreators.php";
-    if(isset($_GET['title'])&& isset($_GET['genre'])&& isset($_GET['author'])&& isset($_GET['release_date'])&&isset($_GET['lang'])&& isset($_GET['ISBN'])){
-        return CreateListedBooksElements($_GET['title'], $_GET['genre'], $_GET['author'],$_GET['release_date'], $_GET['lang'], $_GET['ISBN']);
-    }
-?>
