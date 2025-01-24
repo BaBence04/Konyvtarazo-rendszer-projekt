c258d2f81699a210fe86cc63dae7e70a1ff547ff
hnblnt <hortibalint@gmail.com>
2025-01-24 13:04:50 +0100

    Update tesztadatok2.sql

--- BACKEND/tesztadatok2.sql
+++ BACKEND/tesztadatok2.sql
@@ -1,31 +1,31 @@
--- Adatok beszúrása a languages táblába
+-- Adatok beszúrasa a languages tablaba
 INSERT INTO languages (lang) VALUES
 ('Magyar'),
 ('Angol'),
-('Német'),
+('Nemet'),
 ('Francia'),
-('Japán');
+('Japan');
 
--- Adatok beszúrása a publishers táblába
+-- Adatok beszurasa a publishers tablaba
 INSERT INTO publishers (name, phone, email, webpage) VALUES
-('Kossuth Kiadó', '123-456-7890', 'info@kossuth.hu', 'https://www.kossuth.hu'),
-('Libri Kiadó', '987-654-3210', 'kapcsolat@libri.hu', 'https://www.libri.hu'),
-('Alexandra Kiadó', '555-123-4567', 'ugyfelszolgalat@alexandra.hu', 'https://www.alexandra.hu'),
-('Helikon Kiadó', '444-987-1234', 'info@helikon.hu', 'https://www.helikon.hu'),
-('Animus Kiadó', '333-555-9876', 'kapcsolat@animus.hu', 'https://www.animus.hu');
+('Kossuth Kiado', '123-456-7890', 'info@kossuth.hu', 'https://www.kossuth.hu'),
+('Libri Kiado', '987-654-3210', 'kapcsolat@libri.hu', 'https://www.libri.hu'),
+('Alexandra Kiado', '555-123-4567', 'ugyfelszolgalat@alexandra.hu', 'https://www.alexandra.hu'),
+('Helikon Kiado', '444-987-1234', 'info@helikon.hu', 'https://www.helikon.hu'),
+('Animus Kiado', '333-555-9876', 'kapcsolat@animus.hu', 'https://www.animus.hu');
 
--- Adatok beszúrása a genres táblába
+-- Adatok beszurasa a genres tablaba
 INSERT INTO genres (genre) VALUES
-('Regény'),
-('Tudományos'),
+('Regeny'),
+('Tudomanyos'),
 ('Sci-fi'),
 ('Fantasy'),
 ('Krimi'),
 ('Romantikus'),
 ('Horror'),
-('Életrajz');
+('Eletrajz');
 
--- Adatok beszúrása az authors táblába
+-- Adatok beszurasa az authors tablaba
 INSERT INTO authors (author) VALUES
 ('J.K. Rowling'),
 ('George R.R. Martin'),
@@ -36,7 +36,7 @@ INSERT INTO authors (author) VALUES
 ('Mark Twain'),
 ('Ernest Hemingway');
 
--- Adatok beszúrása a book táblába
+-- Adatok beszurasa a book tablaba
 INSERT INTO book (ISBN, title, publisher_id, release_date, lang_id, description, picture_base64) VALUES
 ('9781234567897', 'Book One', 1, '2020-01-15', 1, 'A compelling fictional story.', "https://unsplash.it/300/450"),
 ('9789876543210', 'Book Two', 2, '2019-07-23', 2, 'An insightful non-fiction book.', "https://unsplash.it/300/450"),
@@ -54,30 +54,30 @@ INSERT INTO book (ISBN, title, publisher_id, release_date, lang_id, description,
 ('9789999999999', 'Book Fourteen', 3, '2020-12-10', 2, 'A chilling horror story with supernatural elements.', "https://unsplash.it/300/450"),
 ('9781010101010', 'Book Fifteen', 1, '2022-07-14', 5, 'A biography of a renowned scientist.', "https://unsplash.it/300/450"),
 
-('9781234567887', 'Első könyv', 1, '2020-01-15', 1, 'Egy lebilincselő regény.', "https://unsplash.it/300/450"),
-('9789876541210', 'Második könyv', 2, '2019-07-23', 2, 'Egy gondolatébresztő tudományos könyv.', "https://unsplash.it/300/450"),
-('9785556667770', 'Harmadik könyv', 3, '2021-03-05', 3, 'Egy epikus sci-fi történet.', "https://unsplash.it/300/450"),
-('2784445556667', 'Negyedik könyv', 4, '2022-10-12', 4, 'Egy lenyűgöző fantasy kaland.', "https://unsplash.it/300/450"),
-('9783334445552', 'Ötödik könyv', 5, '2018-05-19', 5, 'Egy izgalmas krimi regény.', "https://unsplash.it/300/450"),
-('9781111131111', 'Hatodik könyv', 2, '2023-01-20', 1, 'Egy romantikus történet kontinenseken át.', "https://unsplash.it/300/450"),
-('9782222122222', 'Hetedik könyv', 4, '2021-09-10', 4, 'Egy horror történet, ami ébren tart.', "https://unsplash.it/300/450"),
-('9783343333333', 'Nyolcadik könyv', 1, '2020-06-15', 2, 'Egy híres történelmi alak életrajza.', "https://unsplash.it/300/450"),
-('9784444344444', 'Kilencedik könyv', 3, '2019-11-22', 3, 'Egy sci-fi remekmű a mesterséges intelligenciáról.', "https://unsplash.it/300/450"),
-('9785552555555', 'Tizedik könyv', 5, '2022-04-05', 5, 'Egy kalandos fantasy egy varázslatos világban.', "https://unsplash.it/300/450");
+('9781234567887', 'Elso konyv', 1, '2020-01-15', 1, 'Egy lebilincselo regeny.', "https://unsplash.it/300/450"),
+('9789876541210', 'Masodik konyv', 2, '2019-07-23', 2, 'Egy gondolatebreszto tudomanyos konyv.', "https://unsplash.it/300/450"),
+('9785556667770', 'Harmadik konyv', 3, '2021-03-05', 3, 'Egy epikus sci-fi tortenet.', "https://unsplash.it/300/450"),
+('2784445556667', 'Negyedik konyv', 4, '2022-10-12', 4, 'Egy lenyugozo fantasy kaland.', "https://unsplash.it/300/450"),
+('9783334445552', 'otodik konyv', 5, '2018-05-19', 5, 'Egy izgalmas krimi regeny.', "https://unsplash.it/300/450"),
+('9781111131111', 'Hatodik konyv', 2, '2023-01-20', 1, 'Egy romantikus tortenet kontinenseken at.', "https://unsplash.it/300/450"),
+('9782222122222', 'Hetedik konyv', 4, '2021-09-10', 4, 'Egy horror tortenet, ami ebren tart.', "https://unsplash.it/300/450"),
+('9783343333333', 'Nyolcadik konyv', 1, '2020-06-15', 2, 'Egy hires tortenelmi alak eletrajza.', "https://unsplash.it/300/450"),
+('9784444344444', 'Kilencedik konyv', 3, '2019-11-22', 3, 'Egy sci-fi remekmu a mesterseges intelligenciarol.', "https://unsplash.it/300/450"),
+('9785552555555', 'Tizedik konyv', 5, '2022-04-05', 5, 'Egy kalandos fantasy egy varazslatos vilagban.', "https://unsplash.it/300/450");
 
 
--- Adatok beszúrása a books_genres_conn táblába
+-- Adatok beszurasa a books_genres_conn tablaba
 INSERT INTO books_genres_conn (ISBN_id, genre_id) VALUES
-(1, 1), -- Regény
+(1, 1), -- Regeny
 (1, 2),
 (1, 3),
-(2, 2), -- Tudományos
+(2, 2), -- Tudomanyos
 (3, 3), -- Sci-fi
 (4, 4), -- Fantasy
 (5, 5), -- Krimi
 (6, 6), -- Romantikus
 (7, 7), -- Horror
-(8, 8), -- Életrajz
+(8, 8), -- eletrajz
 (9, 3), -- Sci-fi
 (10, 4),
 (11, 2),
@@ -96,7 +96,7 @@ INSERT INTO books_genres_conn (ISBN_id, genre_id) VALUES
 (24, 2),
 (25, 2);
 
--- Adatok beszúrása a books_authors_conn táblába
+-- Adatok beszurasa a books_authors_conn tablaba
 INSERT INTO books_authors_conn (ISBN_id, author_id) VALUES
 (1, 1), -- J.K. Rowling
 (2, 6), -- Jane Austen
@@ -134,16 +134,16 @@ INSERT INTO system_settings (membership_fee, borrowing_time, lengthening_time, r
 
 
 INSERT INTO users (surname, first_name, username, birth_date, membership_end_date, email, phone_number, password, birth_place, address, mother_maiden_name, active) VALUES
-('Kovács', 'János', '7584937648', '1985-03-15', '2025-03-15', 'janos.kovacs@example.com', '06123456789', 'password', 'Budapest', '123 Main St', 'Nagy Erzsébet', TRUE),
-('K.', 'Anna', '0123456789', '1999-10-12', '2025-10-15', 'k.anna@gmail.com', '06302166102', 'password', 'Vác', '123 Váci St', 'Kipuf Ogó', TRUE),
-('Szabó', 'Anna', '6453957465', '1990-10-22', '2025-10-22', 'anna.szabo@example.com', '06789123456', 'password', 'Debrecen', '456 High St', 'Kiss Júlia', TRUE);
+('Kovacs', 'Janos', '7584937648', '1985-03-15', '2025-03-15', 'janos.kovacs@example.com', '06123456789', 'password', 'Budapest', '123 Main St', 'Nagy Erzsebet', TRUE),
+('K.', 'Anna', '0123456789', '1999-10-12', '2025-10-15', 'k.anna@gmail.com', '06302166102', 'password', 'Vac', '123 Vaci St', 'Kipuf Ogo', TRUE),
+('Szabo', 'Anna', '6453957465', '1990-10-22', '2025-10-22', 'anna.szabo@example.com', '06789123456', 'password', 'Debrecen', '456 High St', 'Kiss Julia', TRUE);
 
 INSERT INTO inventory (ISBN_id, book_id,available) VALUES
 (1,1,TRUE),(2,2,TRUE),(3,3,TRUE),(4,4,TRUE),(5,5,TRUE),(6,6,TRUE),(7,7,TRUE),(8,8,TRUE),(9,9,TRUE),(10,10,TRUE),(11,11,TRUE),(12,12,TRUE),(13,13,TRUE),(14,14,TRUE),(15,15,TRUE),(16,16,TRUE),(17,17,TRUE),(18,18,TRUE),(19,19,TRUE),(20,20,TRUE),(21,21,TRUE),(22,22,TRUE),(23,23,TRUE),(24,24,TRUE),(25,25,TRUE);
 
 INSERT INTO employees (empl_name, empl_uname, password) VALUES
-('Tóth Péter', 'AABBC', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19'),
-('Nagy László', 'BBCCD', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19');
+('Toth Peter', 'AABBC', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19'),
+('Nagy Laszlo', 'BBCCD', '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19');
 
 INSERT INTO reservation (ISBN_id, user_id) VALUES
 (1, 1),
@@ -177,4 +177,4 @@ INSERT INTO borrowings (book_id, user_id, empl_e, start_date, end_date, returned
 
 (2, 2, 2, '2024-02-01', '2024-02-16', 1);
 
--- a 3-as könyv-ön lehet tesztelni azt, hogy visszahozáskor átteszi-e a program a booking-ba a 2-es ember reservation-ét(, és értesíti-e)
+-- a 3-as könyv-ön lehet tesztelni azt, hogy visszahozaskor atteszi-e a program a booking-ba a 2-es ember reservation-ét(, és értesíti-e)
