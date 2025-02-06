344b1be67476ec8747945fa338dae729b9c8e1f9
hnblnt <hortibalint@gmail.com>
2025-02-06 13:32:37 +0100

    Update resetPassword.php

--- WEB/resetPassword.php
+++ WEB/resetPassword.php
@@ -21,43 +21,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset password</title>
     <script src="jquery.js"></script>
+    <link rel="stylesheet" href="/web/style.css">
 </head>
 <body>
     <h1>Majd email-ben fog jönni a link</h1>
     <label for="newPassword">Új jelszó:</label>
-    <input required type="password" name="newPassword" id="newPassword"><br>
+    <input type="password" id="newPassword" oninput="ValidatePassword(this.value);">
+    <div id="errors_with_password"></div>
     <label for="newPasswordAgain">Új jelszó újra:</label>
     <input required type="password" id="newPasswordAgain">
     <button onclick="ChangePassword();" type="button">Cserélés</button>
 </body>
+<script src="/web/passwordValidation.js"></script>
 
 <script>
     function ChangePassword(){
-        let pw =document.getElementById("newPassword").value;
-        let pwAgain =document.getElementById("newPasswordAgain").value;
-        if(pw == ""){
-            alert("Adjon meg valamit jelszónak!");
-            return;
-        }if(pw != pwAgain){
-            alert("Nem egyezik a kettő jelszó!");
-            return;
-        }else{
-            const currentUrl = new URL(window.location);
-        
-            $.ajax({
-            url: "../BACKEND/api.php",
-            type: "POST", //send it through post method
-            data: { 
-                reset_token: currentUrl.searchParams.get("reset_token"),
-                password: pw
-            },
-            success: function(response)  {
-                let data =JSON.parse(response);
-                console.log(data);
+        let password =document.getElementById("newPassword").value;
+        let passwordAgain =document.getElementById("newPasswordAgain").value;
+        ValidatePassword(password);
+        if(document.getElementById("errors_with_password").innerHTML == ""){
+            if(password != passwordAgain){
+                alert("Nem egyezik a kettő jelszó!");
+                return;
+            }else{
+                const currentUrl = new URL(window.location);
+            
+                $.ajax({
+                url: "../BACKEND/api.php",
+                type: "POST", //send it through post method
+                data: { 
+                    reset_token: currentUrl.searchParams.get("reset_token"),
+                    password: password
+                },
+                success: function(response)  {
+                    let data =JSON.parse(response);
+                    console.log(data);
+    
+                    alert(data["status"]);
+                }}); 
+            }
 
-                alert(data["status"]);
-            }}); 
+        }else{
+            alert("Még vannak hibák a jelszóval!")
         }
+
     }
 </script>
 </html>
