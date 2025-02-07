7c9af2e825ceed2c918ca02e452bb3f11d2743d7
hnblnt <hortibalint@gmail.com>
2025-02-07 19:23:38 +0100

    Update teszt.php

--- BACKEND/teszt.php
+++ BACKEND/teszt.php
@@ -1,51 +1,19 @@
-<?php
-    // Import PHPMailer classes into the global namespace
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teszt</title>
+</head>
+<body>
+    <?php
+        require_once "databaseFunctions.php";
+        require_once "elementCreators.php";
+        require_once "additionalFunctions.php";
+        require_once "mailer.php";
 
-    // Load Composer's autoloader
-    // require 'vendor/autoload.php';
+        send_user_registration_email(1, "hoba123", create_reset_token_and_link(1));
+    ?>
     
-    require 'PHPMailer.php';
-    require 'SMTP.php';
-    require "Exception.php";
-
-    // Create an instance of PHPMailer
-    $mail = new PHPMailer(true);
-
-    $our_email = file_get_contents("gmail_email.txt");
-    $password = file_get_contents("app_password.txt");                  // SMTP password
-    echo $our_email;
-    echo "<br>";
-    echo $password;
-
-    try {
-        // Server settings
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = $our_email;               // SMTP username
-        $mail->Password   = $password;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
-        $mail->Port       = 587;                                    // TCP port to connect to
-        $mail->CharSet    = PHPMailer::CHARSET_UTF8;
-
-        // Recipients
-        $mail->setFrom($our_email, 'Könyvtározó szakdolgozat');      // Sender's email and name
-        $mail->addAddress('hortibalint@gmail.com', 'Blint'); // Add a recipient
-        // $mail->addReplyTo('info@example.com', 'Information');       // Reply-to address
-
-        // Content
-        $mail->isHTML(true);                                        // Set email format to HTML
-        $mail->Subject = 'Here is the subject';                     // Email subject
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>'; // HTML body
-        $mail->AltBody = 'This is the plain text message body';     // Plain text body for non-HTML clients
-
-        // Send the email
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-
-?>
+</body>
+</html>
