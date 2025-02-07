83d05b4410857fcc4b735fc19f3f818fbaaa9d67
hnblnt <hortibalint@gmail.com>
2025-02-07 11:49:26 +0100

    Update additionalFunctions.php

--- BACKEND/additionalFunctions.php
+++ BACKEND/additionalFunctions.php
@@ -9,6 +9,21 @@
         return $token .= $number_to_put_to_end;
     }
 
+    function send_reset_email(){
+    
+        
+    }
+
+    function setup_session_cookie() : void {
+        session_set_cookie_params([
+            'lifetime' => 0, // Expire when browser closes
+            'path' => '/',
+            'domain' => '',
+            'httponly' => true, // Prevent JS access
+            'samesite' => 'Strict' // Prevent CSRF
+        ]);
+    }
+
 
 
 
