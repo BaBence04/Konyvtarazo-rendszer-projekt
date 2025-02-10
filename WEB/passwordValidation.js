fd9f27b92a7c39fe472d2437e3b503de7ddd3161
hnblnt <hortibalint@gmail.com>
2025-02-10 13:46:13 +0100

    Update passwordValidation.js

--- WEB/passwordValidation.js
+++ WEB/passwordValidation.js
@@ -4,72 +4,35 @@ let updateErrorsTimeout;
 function ValidatePassword(password){
     clearTimeout(updateErrorsTimeout);
 
-    const errorHolder = document.getElementById("errors_with_password");
     const duration = 250;
 
     updateErrorsTimeout = setTimeout(() => {
-        if(password == ""){
-            previousErrorsHtml = "";
-            if(errorHolder.innerHTML != ""){
-                $(errorHolder).slideUp(duration, "swing");
-                errorHolder.setAttribute("data-open", "0");
-                setTimeout(() => {
-                    errorHolder.innerHTML = "";
-                }, duration);
-            }
-            return;
-        }
-
-
         const checks = [
-            { regex: /.{8,}/, message: "A jelszónak legalább 8 karakter hosszúnak kell lennie!" },
-            { regex: /[a-z]/, message: "A jelszónak kell kis betűt tartalmaznia!" },
-            { regex: /[A-Z]/, message: "A jelszónak kell nagy betűt tartalmaznia!" },
-            { regex: /\d/, message: "A jelszónak kell számot tartalmaznia!" },
-            { regex: /[@$!%*?&.,=()\'\"]/, message: "A jelszónak kell speciális karaktert tartalmaznia(@$!%*?&.,=()\'\")!" },
+            { regex: /.{8,}/, id: "error_password_short" },
+            { regex: /[a-z]/, id: "error_no_small_char" },
+            { regex: /[A-Z]/, id: "error_no_big_char" },
+            { regex: /\d/, id: "error_no_digit" },
+            { regex: /[@$!%*?&.,=()\'\"]/, id: "error_no_special_char" },
         ];
     
         // Test the password against each check
-        const errors = checks.filter(check => !check.regex.test(password)).map(check => check.message);
+        const errors = checks.filter(check => !check.regex.test(password)).map(check => check.id);
+
+        const passed_checks = checks.filter(check=> !errors.includes(check.id)).map(check=>check.id);
         
-    
-        // Display errors to the user
-        if (errors.length > 0) {
-            let errorsHtml = "";
-            errors.forEach(error => errorsHtml += `<p>${error}</p>`);
-    
-            if(errorsHtml != previousErrorsHtml){
-                jQuery.easing.def = "easeOutCubic";
-                if (errorHolder.getAttribute("data-open") == "1") {
-                    $(errorHolder).slideUp(duration, "swing");
-                    errorHolder.setAttribute("data-open", "0");
-                    setTimeout(() => {
-                        errorHolder.innerHTML = errorsHtml;
-                        $(errorHolder).slideDown(duration, "swing");
-                        errorHolder.setAttribute("data-open", "1");
-        
-                    }, duration);
-                    
-                } else {
-                    $(errorHolder).slideUp(0, "swing");
-                    errorHolder.innerHTML = errorsHtml;
-                    $(errorHolder).slideDown(duration, "swing");
-                    errorHolder.setAttribute("data-open", "1");
-                }
-        
-        
-                errorHolder.innerHTML = errorsHtml;
-                previousErrorsHtml = errorsHtml;
-            }
-        } else {
-            if(errorHolder.innerHTML != ""){
-                $(errorHolder).slideUp(duration, "swing");
-                errorHolder.setAttribute("data-open", "0");
-                setTimeout(() => {
-                    errorHolder.innerHTML = "";
-                }, duration);
-            }
+        if(errors.length > 0 && password != ""){
+            passed_checks.forEach(id => {
+                $(`#${id}`).hide(duration);
+            });
+
+            errors.forEach(id=>{
+                $(`#${id}`).show(duration);
+            });
+            
+        }else{
+            $("#errors_with_password>p").hide(duration);
         }
 
     }, 300);
 }
+
