421518a7abbb13ac2602d71eeb04ac8d0ffce88f
hnblnt <hortibalint@gmail.com>
2025-03-06 11:17:13 +0100

    Update elementCreators.php

--- BACKEND/elementCreators.php
+++ BACKEND/elementCreators.php
@@ -1,24 +1,25 @@
 <?php
     // require_once "databaseFunctions.php";
-    function CreateBookElement($data):string{
+    function CreateBookElement($data, $checked = false):string{
         $result = "
             <div class='grid-item' data-isbn='$data[ISBN]' onclick='OpenBookDetailed(this);'>
                 <img src='".$data["picture_base64"]."' alt='".$data["title"]." könyv borítóképe'>
                 <div class='book-title'>".$data["title"]."</div>
                 <div class='book-author'>".str_replace(",",", ",$data["authors"])."</div>
 
-                <div class='book-genres'>".'#' . str_replace(",", ", #", $data["genres"])."</div>
+                <div class='book-genres'>".'#' . str_replace(",", ", #", $data["genres"])."</div>";
 
-                
+        if(isset($_SESSION["user_id"])){
+            $result .="<label class='ui-like'>
+                        <input type='checkbox'".($checked?" checked ":"")." onchange='addOrRemoveFromShelf(this);' data-isbn-id='$data[ISBN_id]' class='like-input'>
+                        <div class='like'>
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill=''><g stroke-width='0' id='SVGRepo_bgCarrier'></g><g stroke-linejoin='round' stroke-linecap='round' id='SVGRepo_tracerCarrier'></g><g id='SVGRepo_iconCarrier'><path d='M20.808,11.079C19.829,16.132,12,20.5,12,20.5s-7.829-4.368-8.808-9.421C2.227,6.1,5.066,3.5,8,3.5a4.444,4.444,0,0,1,4,2,4.444,4.444,0,0,1,4-2C18.934,3.5,21.773,6.1,20.808,11.079Z'></path></g></svg>
+                        </div>
+                    </label>";
+        }
 
-                <label class='ui-like'>
-                    <input type='checkbox' class='like-input'>
-                    <div class='like'>
-                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill=''><g stroke-width='0' id='SVGRepo_bgCarrier'></g><g stroke-linejoin='round' stroke-linecap='round' id='SVGRepo_tracerCarrier'></g><g id='SVGRepo_iconCarrier'><path d='M20.808,11.079C19.829,16.132,12,20.5,12,20.5s-7.829-4.368-8.808-9.421C2.227,6.1,5.066,3.5,8,3.5a4.444,4.444,0,0,1,4,2,4.444,4.444,0,0,1,4-2C18.934,3.5,21.773,6.1,20.808,11.079Z'></path></g></svg>
-                    </div>
-                </label>
-            </div>
-        ";
+
+        $result.="</div>";
 
         return $result;
     }
@@ -28,13 +29,24 @@
         $resultHTML="";
         // $data = GetBooksFiltered($_GET["title"],$_GET["genre"],$_GET["author"],$_GET["release_date"],$_GET["lang"],$_GET["isbn"]);
         $data = get_books_filtered($title,$genre,$author,$release_date,$lang,$isbn, $page);
-        $number_of_results = get_books_filtered_number_of_results($title,$genre,$author,$release_date,$lang,$isbn);
         
+        if(isset($_SESSION["user_id"])){
+            $users_favorite_books = get_users_favorite_books($_SESSION["user_id"]);
+            $favorite_books_isbn_ids = array_map(function($record){
+                return $record["ISBN_id"];
+            }, $users_favorite_books);
+        }
+
+        $number_of_results = get_books_filtered_number_of_results($title,$genre,$author,$release_date,$lang,$isbn);
         $last_possible_page = ceil($number_of_results/$results_per_page);
 
         $resultHTML.="<div class='content_container grid-container'>";
         for ($i=0; $i < count($data); $i++) { 
-            $resultHTML.=CreateBookElement($data[$i]);
+            $checked = false;
+            if(isset($_SESSION["user_id"]) && in_array($data[$i]["ISBN_id"], $favorite_books_isbn_ids)){
+                $checked = true;
+            }
+            $resultHTML.=CreateBookElement($data[$i], $checked);
         }
         if(count($data)==0){
             if($last_possible_page<$page && $page != 1){
